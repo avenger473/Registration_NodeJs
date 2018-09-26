9e2239ca259f9f68e5cbb09ee492669d0ba09505
Aman Kumar Singh <er.aks473@gmail.com>
2018-09-26 09:51:47 +0530

    Version 3.0

--- controller/registercontroller.js
+++ controller/registercontroller.js
@@ -1,5 +1,7 @@
 var mongo= require('mongoose');
 mongo.connect('mongodb://localhost:27017/register');
+var JSAlert = require("js-alert");
+ 
 
 var bp= require('body-parser');
 
@@ -30,10 +32,12 @@ app.get('/register', function(req, res){
 
 app.post('/register',urlep, function(req, res){
 	
-	console.log(req.body);
+	//console.log(req.body);
 	
 	var newReg = Register({name: req.body.name, email: req.body.email, phone: req.body.phone, dob: req.body.date}).save(function(err){
 	if(err) throw err;
+	JSAlert.alert("This is an alert.");
+	//res.write('Added');
 	res.render('home');
 	});
 });
@@ -47,8 +51,8 @@ app.get('/seeregister', function(req, res){
 	});	
 });
 
-app.delete('/seeregister/:email', function(req, res){
-	Register.find({email: req.params.email}).remove(function(err, data){
+app.delete('/seeregister/:id', function(req, res){
+	Register.find({_id: req.params.id}).remove(function(err, data){
 		
 		if(err) throw err;
 		res.json(data);
@@ -56,5 +60,32 @@ app.delete('/seeregister/:email', function(req, res){
 
 });
 
+app.get('/editregister/:id', function(req, res){
+	
+	Register.find({_id: req.params.id}, function(err, data){
+		
+		if(err) throw err;
+		//console.log(data);
+		res.render('edit', {record: data[0]});
+	});	
+});
+
+app.post('/editregister',urlep, function(req, res){
+	
+	console.log(req.body);
+
+	Register.findOneAndUpdate({"_id": req.body.id}, 
+		{"name" : req.body.name, 
+		"email" : req.body.email, 
+		"phone": req.body.phone, 
+		"dob": req.body.date}, 
+		{upsert:false}, function(err, data){
+			if(err) throw err;
+			console.log(data);
+			res.redirect('/seeregister');
+
+		});
+
+});
 
 };
